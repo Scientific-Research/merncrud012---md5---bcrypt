c9fc534b11c9243a352238cfa26949d847c32aad
Maximilian <scientificresearch309@gmail.com>
2023-06-10 13:44:56 +0200

    finished!

--- bcrypt.js
+++ bcrypt.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcrypt';
 
 const register = async (password) => {
-	const salt = await bcrypt.genSalt(16);
+	const salt = await bcrypt.genSalt(12);
 	// console.log(salt);
 	const hash = await bcrypt.hash(password, salt);
 	console.log(hash); // store hash in database in user's record
 };
 
-// register('ölkölk');
+register('3$GoalEasy');
 
 const login = async (password) => {
-	const hash = '$2b$16$bGw4URlT02ODtdovLNQKROsULtsx7eFQAexrTZ9g7PoU7xF17EtQK'; // get hash from user record in database
+	const hash = '$2b$12$z5gAforSyljDotA.wrSYnO/ULIuCifHt6/.4hIvjX759XHPJP7Iv6'; // get hash from user record in database
 	// console.log('hash ', hash);
 	const result = await bcrypt.compare(password, hash);
 	console.log(result);
 };
 
-login('ölkölk');
+login('3$GoalEasy');
 
 // register('test111');
 // login('test000'); // false
